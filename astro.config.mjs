bf8a6d74d2ba84642dd918070fad9b70abaeafe7
unsecretised <no1umang@gmail.com>
2026-09-11 11:44:29 +0800

    did you join the discord

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  output: 'server',
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
